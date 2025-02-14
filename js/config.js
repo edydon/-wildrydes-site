c14a40530c417dde1787ccec51d6bfd1f3be9d2a
edydon <48735782+edydon@users.noreply.github.com>
2025-02-14 18:16:30 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,6 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-1_WLU71HpEn', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '2m1e81269dmc4t6j0bast8ectf', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientSecret: '1qco6pj6qmohpk6dsin5ep4ii7bfad7uvc2j8ndk7qm2782t756v', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
